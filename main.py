d10f2a8040eb632908f5c3021b04a27855b25f1e
Iban Montero Martinez <imontero@c4r1s5.42urduliz.com>
2024-04-17 12:15:59 +0200

    m

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
 
-service = Service(executable_path="geckodriver")
+service = Service(executable_path="chromedriver")
 drive = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
 time.sleep(15)
 
-driver.quite()
+driver.quit()
