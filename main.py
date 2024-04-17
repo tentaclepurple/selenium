5c064d3ac1fb94b4a351f74126c509e474fe089e
Iban Montero Martinez <imontero@c4r1s5.42urduliz.com>
2024-04-17 12:20:02 +0200

    m

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
 
-service = Service(executable_path="gekodriver")
+service = Service(executable_path="geckodriver")
 driver = webdriver.Firefox(service=service)
 
 driver.get("https://google.com")
